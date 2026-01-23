ed97144c846ba885399782b6653b880ab1b20258
danicruzi <dani.cruz0207@gmail.com>
2026-01-22 22:11:45 -0600

    Add command to list MySQL databases
    List databases in MySQL using a command.

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,1 +1,7 @@
 cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
+Enter password: 
+Database                                                                                     
+information_schema                                                                           
+mysql                                                                                        
+performance_schema                                                                           
+sys        
