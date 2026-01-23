eef92927f930dbd2f46b3acfdc3cf866c8d4d4a2
danicruzi <dani.cruz0207@gmail.com>
2026-01-22 22:20:55 -0600

    Update 0-list_databases.sql

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,9 +1,3 @@
 --text
 SHOW DATABASES; 
-cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
-Enter password: 
-Database                                                                                     
-information_schema                                                                           
-mysql                                                                                        
-performance_schema                                                                           
-sys   
+
