5e3d628c9838dd7a29ef8714489579c257d6fc19
danicruzi <dani.cruz0207@gmail.com>
2026-01-22 23:22:21 -0600

    Update 16-no_link.sql

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,1 +1,5 @@
-
+-- script that lists all records of the table second_table where name is not null and not empty
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
