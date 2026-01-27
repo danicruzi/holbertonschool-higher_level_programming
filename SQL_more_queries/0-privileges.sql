2f5da6bd3595293d4d699b97dfda331e662d0ab2
danicruzi <dani.cruz0207@gmail.com>
2026-01-26 19:44:28 -0600

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- List all privileges for user_0d_1 and user_0d_2
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
