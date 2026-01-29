e600c89d7874e9cc956852a9a838a6556a344779
danicruzi <dani.cruz0207@gmail.com>
2026-01-29 13:53:13 -0600

    Update 5-unique_id.sql

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
--- creates table on server 
-CREATE IF NOT EXISTS unique_id(
-  id INT NOT NULL UNIQUE DEFAULT 1,
-  name VARCHAR(256)
+-- script creates table on server
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT NOT NULL UNIQUE DEFAULT 1,
+    name VARCHAR(256)
 );
